289eb1f17c14ed3cbf74c71910f4473b02ff5d68
shweta-sakshi <shweta25sakshi@gmail.com>
2024-10-30 05:41:16 +0530

    add proxy for backend in frontend and setup socket connection.

--- backend/Server.js
+++ backend/Server.js
@@ -2,15 +2,35 @@ const express = require('express')
 const mongoose = require('mongoose');
 require('dotenv').config()
 const cors = require("cors");
-
 const app = express()
-const port = 3000
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+// Enable CORS specifically for Socket.IO
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173", // Allow requests from this origin
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
+//cros for express.
 app.use(cors());
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+//connection event
+io.on("connection", (socket) => {
+    console.log("Client connected:", socket.id);
+    socket.on("disconnect", () => console.log("Client disconnected:", socket.id));
+});
 
+const port = 3000
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+// database connection.
 mongoose.connect(process.env.DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
