3f532abe62e999ee2c38de4ca96759383036fb49
priyankaKri-01 <priyankayaduvanshi08@gmail.com>
2024-11-08 22:56:43 +0530

    correction of frontend route for landingPage.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './HomePage'
-import Nav from './Nav.jsx'
 import Login from './Login.jsx'
 import Signup from './Signup.jsx'
 import Activationpage from './Activationpage.jsx'
@@ -19,12 +18,11 @@ function App() {
       <BrowserRouter>
       
         <Routes>
-          <Route path='/' element={<Nav />} />
+          <Route path='/' element={<LandingPage/>} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Signup />} />
           <Route path='/activation/:activation_token' element={<Activationpage />} />
           <Route path='/homePage' element={<HomePage />} />
-          <Route path='/landingPage' element={<LandingPage/>}/>
           <Route path='/eventPage' element={<EventPage/>}/>
           <Route path='/gamePage' element={<GamePage/>}/>
           <Route path='/gameInfo' element={<GameInfo/>}/>
